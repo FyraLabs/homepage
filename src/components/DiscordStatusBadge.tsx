6ed33d7433817d3f9f6773f374a1530bea6d595b
Lleyton Gray <lleyton@fyralabs.com>
2023-02-12 18:28:54 -0800

    various fixes

--- src/components/DiscordStatusBadge.tsx
+++ src/components/DiscordStatusBadge.tsx
@@ -22,12 +22,12 @@ const DiscordStatusBadge: React.FC<{
       className={classNames(
         "tooltip w-8 h-8 rounded-full border-4 absolute right-4 bottom-0.5",
         status === "online" || (dndIsOnline && status === "dnd")
-          ? "bg-emerald-500 border-emerald-600"
+          ? "bg-emerald-500 border-emerald-600 dark:bg-emerald-400 dark:border-emerald-500"
           : status === "idle"
-          ? "bg-yellow-500 border-yellow-600"
+          ? "bg-yellow-500 border-yellow-600 dark:bg-amber-300 dark:border-amber-400"
           : status === "dnd"
-          ? "bg-red-500 border-red-600"
-          : "bg-gray-500 border-gray-600"
+          ? "bg-red-500 border-red-600 dark:bg-red-400 dark:border-red-500"
+          : "bg-gray-500 border-gray-600 dark:bg-gray-300 dark:border-gray-400"
       )}
     />
   );
