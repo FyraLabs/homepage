76026a174822a62a75dd9763594350efc78e1f2b
Lleyton Gray <lleyton@fyralabs.com>
2023-02-12 17:23:29 -0800

    some fixes

--- scripts/map.ts
+++ scripts/map.ts
@@ -1,50 +1,53 @@
-import DottedMap from "dotted-map";
+import DottedMap from "@lleyton/dotted-map";
 
 const pins = [
   {
     lat: 34.05,
     lng: -118.25,
-    svgOptions: { color: "#ec4899", radius: 0.4 },
+    svgOptions: { color: "#ec4899", radius: 0.6 },
   },
   {
     lat: 44.94417,
     lng: -93.09361,
-    svgOptions: { color: "#a855f7", radius: 0.4 },
+    svgOptions: { color: "#a855f7", radius: 0.6 },
   },
   {
     lat: -23.55,
     lng: -46.633,
-    svgOptions: { color: "#a855f7", radius: 0.4 },
+    svgOptions: { color: "#a855f7", radius: 0.6 },
   },
   {
     lat: 13.7525,
     lng: 100.49417,
-    svgOptions: { color: "#a855f7", radius: 0.4 },
+    svgOptions: { color: "#a855f7", radius: 0.6 },
   },
   {
     lat: 22.3193,
     lng: 114.1694,
-    svgOptions: { color: "#a855f7", radius: 0.4 },
+    svgOptions: { color: "#a855f7", radius: 0.6 },
   },
   {
     lat: 51.1657,
     lng: 10.4515,
-    svgOptions: { color: "#a855f7", radius: 0.4 },
+    svgOptions: { color: "#a855f7", radius: 0.6 },
   },
   {
     lat: 6.2088,
     lng: 106.8456,
-    svgOptions: { color: "#a855f7", radius: 0.4 },
+    svgOptions: { color: "#a855f7", radius: 0.6 },
   },
   {
     lat: 47.5162,
     lng: 14.5501,
-    svgOptions: { color: "#a855f7", radius: 0.4 },
+    svgOptions: { color: "#a855f7", radius: 0.6 },
   },
 ];
 
 // @ts-expect-error
-const map = new DottedMap.default({ height: 40, grid: "vertical" });
+const map = new DottedMap.default({
+  height: 40,
+  grid: "vertical",
+});
 pins.forEach((pin) => map.addPin(pin));
 
 console.log(
