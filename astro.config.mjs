4524929d1525161382d5f38a76050f9661630e27
Lleyton Gray <lleyton@fyralabs.com>
2024-01-30 03:12:11 -0800

    chore: upgrade deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,16 @@
 import { defineConfig } from "astro/config";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import react from "@astrojs/react";
-
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
 import compress from "astro-compress";
-
-// https://astro.build/config
 import robotsTxt from "astro-robots-txt";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://fyralabs.com",
   integrations: [
+    icon(),
     tailwind(),
     react(),
     sitemap({ filter: (page) => page !== "https://fyralabs.com/thanks/" }),
--- src/components/DiscordStatusBadge.tsx
+++ src/components/DiscordStatusBadge.tsx
@@ -24,10 +24,10 @@ const DiscordStatusBadge: React.FC<{
         status === "online" || (dndIsOnline && status === "dnd")
           ? "bg-emerald-500 border-emerald-600 dark:bg-emerald-400 dark:border-emerald-500"
           : status === "idle"
-          ? "bg-yellow-500 border-yellow-600 dark:bg-amber-300 dark:border-amber-400"
-          : status === "dnd"
-          ? "bg-red-500 border-red-600 dark:bg-red-400 dark:border-red-500"
-          : "bg-gray-500 border-gray-600 dark:bg-gray-300 dark:border-gray-400",
+            ? "bg-yellow-500 border-yellow-600 dark:bg-amber-300 dark:border-amber-400"
+            : status === "dnd"
+              ? "bg-red-500 border-red-600 dark:bg-red-400 dark:border-red-500"
+              : "bg-gray-500 border-gray-600 dark:bg-gray-300 dark:border-gray-400",
       )}
     />
   );
