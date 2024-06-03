d9eeb06558bfc09bd3435413278607f6be64644a
lleyton@fyralabs.com <lleyton@fyralabs.com>
2024-06-02 21:26:11 -0700

    feat: turnstile for contact

--- functions/api/submit.ts
+++ functions/api/submit.ts
@@ -1,49 +1,61 @@
+import turnstilePlugin from "@cloudflare/pages-plugin-turnstile";
+
 interface Env {
   DISCORD_WEBHOOK_URL?: string;
+  TURNSTILE_SECRET?: string;
 }
 
-export const onRequestPost: PagesFunction<Env> = async (context) => {
-  const data = await context.request.formData();
-  const email = data.get("email");
-  const message = data.get("message");
-  if (
-    !(
-      email &&
-      message &&
-      email.length > 0 &&
-      message.length > 0 &&
-      email.length <= 120 &&
-      message.length <= 600
+export const onRequestPost: PagesFunction<Env>[] = [
+  (context) =>
+    // TURNSTILE_SECRET may not be set in preview deploys
+    context.env.TURNSTILE_SECRET
+      ? turnstilePlugin({
+          secret: context.env.TURNSTILE_SECRET,
+        })(context)
+      : context.next(),
+  async (context) => {
+    const data = await context.request.formData();
+    const email = data.get("email");
+    const message = data.get("message");
+    if (
+      !(
+        email &&
+        message &&
+        email.length > 0 &&
+        message.length > 0 &&
+        email.length <= 120 &&
+        message.length <= 600
+      )
     )
-  )
-    return new Response("Invalid form data", { status: 400 });
+      return new Response("Invalid form data", { status: 400 });
 
-  // DISCORD_WEBHOOK_URL may not be set in preview deploys
-  if ("DISCORD_WEBHOOK_URL" in context.env)
-    await fetch(context.env.DISCORD_WEBHOOK_URL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        content: null,
-        embeds: [
-          {
-            title: "New Contact Form Submission",
-            fields: [
-              {
-                name: "Email",
-                value: email,
-              },
-              {
-                name: "Message",
-                value: message,
-              },
-            ],
-          },
-        ],
-      }),
-    });
+    // DISCORD_WEBHOOK_URL may not be set in preview deploys
+    if ("DISCORD_WEBHOOK_URL" in context.env)
+      await fetch(context.env.DISCORD_WEBHOOK_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          content: null,
+          embeds: [
+            {
+              title: "New Contact Form Submission",
+              fields: [
+                {
+                  name: "Email",
+                  value: email,
+                },
+                {
+                  name: "Message",
+                  value: message,
+                },
+              ],
+            },
+          ],
+        }),
+      });
 
-  return Response.redirect("https://fyralabs.com/thanks", 303);
-};
+    return Response.redirect("https://fyralabs.com/thanks", 303);
+  },
+];
