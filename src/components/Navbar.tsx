7f1180d71cf98b5bdc8d6e574a37e15d8f8ea10f
Lleyton Gray <lleyton@fyralabs.com>
2025-11-30 15:32:00 -0800

    fix warns

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -43,7 +43,7 @@ const Navbar: React.FC<{
                 </Disclosure.Button>
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <a className="flex flex-shrink-0 items-center" href="/">
+                <a className="flex shrink-0 items-center" href="/">
                   <img
                     className="block h-8 w-auto drop-shadow-xl"
                     src="/logo.svg"
