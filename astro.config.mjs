b77bcec8dae0fe481f838dae07ce66601a88f5e7
Lleyton Gray <lleyton@fyralabs.com>
2025-11-30 15:00:16 -0800

    upgrade tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,31 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import compress from "astro-compress";
 import robotsTxt from "astro-robots-txt";
 import icon from "astro-icon";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://fyralabs.com",
+
   integrations: [
     icon(),
-    tailwind(),
     react(),
     sitemap({ filter: (page) => page !== "https://fyralabs.com/thanks/" }),
     compress(),
     robotsTxt(),
   ],
+
   redirects: {
     // why are we not using pages redirects for this?
     // see: https://x.com/uvdotsh/status/1825811082822517213
     "/matrix": "https://blog.fyralabs.com/dropping-matrix/",
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
