0d98c497492ad98d09c836aa3dc4906d9d63175b
Lleyton Gray <lleyton@fyralabs.com>
2025-12-18 15:20:18 -0800

    tweaking

--- src/util/ui.ts
+++ src/util/ui.ts
@@ -1,1 +1,8 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export const classNames = (...args: string[]) => args.filter(Boolean).join(" ");
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
